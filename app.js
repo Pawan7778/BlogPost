58bcd01014f2c51e3e481e85650717c6de400988
Pawan7778 <pawankumar7209442464@gmail.com>
2021-05-29 17:28:38 +0530

    another commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
+const _ = require('lodash')
 
 const homestattingcontent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
 
@@ -44,6 +45,19 @@ app.post('/compose',function(req, res){
     res.redirect('/')
 
 });
+app.get('/posts/:postNames', function(req, res){
+    const reqTitle = _.lowerCase(req.params.postNames);
+    BlogPosts.forEach(post => {
+        const storedTitle = _.lowerCase(post.title);
+        if (storedTitle === reqTitle) {
+            console.log('Match found');
+        }
+        else{
+            console.log('Match not found');
+        }
+        
+    });
+})
 
 
 
