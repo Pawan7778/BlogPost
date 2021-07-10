390f3e159f5e840f6d1ff622518a419c285c407f
Pawan7778 <pawankumar7209442464@gmail.com>
2021-07-10 12:20:39 +0530

    going to post this on heroku

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const _ = require('lodash')
+const mongoose = require('mongoose')
 
 const homestattingcontent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
 
@@ -11,7 +12,17 @@ const contactcontent = " Lorem Ipsum is simply dummy text of the printing and ty
 
 const app = express();
 
-const BlogPosts = [];
+mongoose.connect("mongodb+srv://admin-pawan:Test123@cluster0.w4aix.mongodb.net/blogDB",{useNewUrlParser:true ,useUnifiedTopology: true});
+
+const postSchema = {
+    title : String,
+    content : String
+}
+
+const Post = mongoose.model('Post',postSchema);
+
+
+// const BlogPosts = [];
 
 app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({extended:true}))
@@ -20,10 +31,12 @@ app.use(express.static("public"));
 
 
 app.get('/', function(req, res){
-    res.render('home',{
-        con: homestattingcontent,
-        BP : BlogPosts
-    });
+    Post.find({},function (err,posts){
+        res.render('home',{
+            con : homestattingcontent,
+            BP : posts
+        })
+    })
     
 });
 
@@ -39,30 +52,26 @@ app.get('/compose',function(req, res){
 });
 app.post('/compose',function(req, res){
     
-    const post = {
+    const post =new Post({
         title : req.body.postTitle,
         content : req.body.postBody
-    };
-    BlogPosts.push(post)
+    });
+    post.save();
     
     res.redirect('/')
 
 });
-app.get('/posts/:postNames', function(req, res){
-    const reqTitle = _.lowerCase(req.params.postNames);
-    BlogPosts.forEach(post => {
-        const storedTitle = _.lowerCase(post.title);
-        if (storedTitle === reqTitle){
-            
-            res.render('post', {
-                t: post.title,
-                c: post.content
-            })
-        }
-        
-        
+app.get('/posts/:postId', function(req, res){
+    const requestedPostId = req.params.postId;
+    // const reqTitle = _.lowerCase(req.params.postNames);
+    Post.findOne({_id : requestedPostId},function(err,post){
+        res.render('post',{
+            title : post.title,
+            content : post.content
+        });
     });
-})
+    
+});
 
 
 
@@ -94,3 +103,4 @@ app.listen(3000,function(err) {
     }
     console.log("Server is running on port 3000");
 })
+
